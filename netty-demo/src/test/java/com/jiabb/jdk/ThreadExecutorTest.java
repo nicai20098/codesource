9bdcf505d54f52febe654ac276e450779da5c615
jiabinbin <jia_bin1115@163.com>
2022-03-08 21:45:56 +0800

    完善 ThreadExecutorTest

--- netty-demo/src/test/java/com/jiabb/jdk/ThreadExecutorTest.java
+++ netty-demo/src/test/java/com/jiabb/jdk/ThreadExecutorTest.java
@@ -8,19 +8,25 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class ThreadExecutorTest {
 
+	//AbortPolicy 抛出异常程序不会停止
+	//CallerRunsPolicy 任务回全部计算完成，由主线程来执行该任务
+	//DiscardPolicy  丢弃后来的任务
+	//DiscardOldestPolicy  丢弃最早的任务
 	public static void main(String[] args) {
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(8, 8, 20, TimeUnit.SECONDS,
-				new LinkedBlockingDeque<>(), Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
-		for (int i = 0; i <20 ; i++) {
+		AtomicInteger atomicInteger = new AtomicInteger(0);
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(8, 20, 20, TimeUnit.MILLISECONDS,
+				new ArrayBlockingQueue<>(10), Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
+		for (int i = 0; i <200 ; i++) {
 			executor.execute(new Runnable() {
 				@Override
 				public void run() {
 					try {
-						log.info(Thread.currentThread().getName());
+						log.info(Thread.currentThread().getName() +  "__" +atomicInteger.incrementAndGet());
 						TimeUnit.SECONDS.sleep(5);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
@@ -28,7 +34,9 @@ public class ThreadExecutorTest {
 				}
 			});
 		}
-
+		System.out.println(Thread.currentThread().getName() + "end");
+		System.out.println(executor.getLargestPoolSize());
+		executor.shutdown();
 
 	}
 }
